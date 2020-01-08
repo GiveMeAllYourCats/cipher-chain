df60286ea1d4deb6285196d3cb3f3ccdab82c23d
michaeldegroot <specamps@gmail.com>
2020-01-08 01:52:31 +0100

    tests working

--- tests/mocha/test.js
+++ tests/mocha/test.js
@@ -4,20 +4,39 @@ const path = require('path')
 const cipherChain = require('../../cipher-chain')
 const _ = require('lodash')
 
-describe('cipherchain stuff', function() {
-  before(done => {
-    setTimeout(() => {
-      done()
-    }, 500)
-  })
-  after(done => {
-    setTimeout(() => {
-      done()
-    }, 500)
-  })
+const ciphering = new cipherChain('secretje')
 
-  it('Yeets', done => {
-    console.log('YEeet!')
-    done()
-  })
+describe('algorithm testing', function() {
+  for (let cipher in ciphering.ciphers) {
+    it(cipher, async () => {
+      let encrypted = await ciphering.encrypt('secret data', cipher)
+      let decrypted = await ciphering.decrypt(encrypted)
+      assert.equal(decrypted, 'secret data')
+    })
+  }
+})
+
+describe('chaining testing', function() {
+  const chains = []
+
+  const cipherperpart = 3
+  let ciphercount = 0
+  let tempchain = []
+  for (let cipher in ciphering.ciphers) {
+    tempchain.push(cipher)
+    ciphercount++
+    if (cipherperpart == ciphercount) {
+      chains.push(tempchain)
+      tempchain = []
+      ciphercount = 0
+    }
+  }
+
+  for (let chain of chains) {
+    it(chain.join(' -> '), async () => {
+      let encrypted = await ciphering.encrypt('secret data', chain)
+      let decrypted = await ciphering.decrypt(encrypted)
+      assert.equal(decrypted, 'secret data')
+    })
+  }
 })
